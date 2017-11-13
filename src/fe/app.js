8e1d63038656d777fd0538328bcd3e87e7fd8740
Nathan Carpenter <nathan.carpenter03@gmail.com>
2017-11-12 23:13:53 -0500

    added routes

--- src/fe/app.js
+++ src/fe/app.js
@@ -2,14 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'semantic-ui-css/semantic.min.css';
 import { AppContainer } from 'react-hot-loader';
+import { BrowserRouter as Router } from 'react-router-dom';
 import App from './components/App';
+
 
 const root = document.getElementById('app-container');
 
 const renderApp = () => {
   ReactDOM.render(
     <AppContainer>
-      <App />
+      <Router>
+        <App />
+      </Router>
     </AppContainer>
     , root,
   );
--- src/fe/components/Cms/index.js
+++ src/fe/components/Cms/index.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { Sidebar, Menu, Icon } from 'semantic-ui-react';
 import { Helmet } from 'react-helmet';
 import store from 'store';
+import { Route, Link } from 'react-router-dom';
 import styles from './styles.css';
 import Users from '../Users';
 
@@ -19,16 +20,19 @@ const Cms = () => {
       </Helmet>
 
       <Sidebar as={Menu} inverted visible vertical width="thin" icon="labeled">
-        <Menu.Item name="users">
-          <Icon name="users" />
+        <Link to="/users">
+          <Menu.Item name="users">
+            <Icon name="users" />
           Users
-        </Menu.Item>
+          </Menu.Item>
+        </Link>
         <Menu.Item name="logout" onClick={handleLogout()}>
           <Icon name="power" />
           Logout
         </Menu.Item>
       </Sidebar>
       <div className={styles.mainBody}>
+        <Route path="/users" component="Users" />
         <Users />
       </div>
     </div>
